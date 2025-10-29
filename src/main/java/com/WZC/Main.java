690c06003c50e2013ee479e3ac3877ab4b920c1f
WZC1019 <1281354195@qq.com>
2025-10-29 20:23:51 +0800

    feat: 初始化项目结构和基础配置

---
+++ src/main/java/com/WZC/Main.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Java高级1班Git实践项目启动成功！");
+        System.out.println("这是初始版本提交");
+    }
+}
